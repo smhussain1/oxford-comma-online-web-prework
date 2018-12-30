c3bbcca39f656f8d752eb5f227f60e7d8beba45f
michael hussain <smhussain_1@yahoo.com>
2018-12-30 20:50:36 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,22 @@
 def oxford_comma(array)
+  if array.length == 1 
+    array.join("and")
+  elsif array.length == 2 
+    array.join(" and ")
+  elsif array.length == 3 
+    array_last = array.last 
+    array_one = array.pop
+    string_one = array.join(", ")
+    string_one + (", and ") + array_last 
+  else array.length > 3 
+  array_last = array.last 
+    array_one = array.pop
+    string_one = array.join(", ")
+    string_one + (", and ") + array_last 
+  
+  
+  end 
+  
+
 
 end
