9b9b96221ba5fb1299038879333d9465a71919b9
michael hussain <smhussain_1@yahoo.com>
2019-01-05 20:25:31 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,12 +3,12 @@ def oxford_comma(array)
     array.join("and")
   elsif array.length == 2 
     array.join(" and ")
-  elsif array.length == 3 
-    array_last = array.last 
-    array_one = array.pop
-    string_one = array.join(", ")
-    string_one + (", and ") + array_last 
-  else array.length > 3 
+  # elsif array.length == 3 
+  #   array_last = array.last 
+  #   array_one = array.pop
+  #   string_one = array.join(", ")
+  #   string_one + (", and ") + array_last 
+  else array.length >= 3 
   array_last = array.last 
     array_one = array.pop
     string_one = array.join(", ")
